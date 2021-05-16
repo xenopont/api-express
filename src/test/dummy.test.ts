1f3e391ee41008c6d74c2b8f134cab42352f0b39
Sergei Kovalenko <sergei.kovalenko@gamigo.com>
2021-05-16 17:51:11 +0200

    eslint, tests

--- src/test/dummy.test.ts
+++ src/test/dummy.test.ts
@@ -4,10 +4,10 @@ import 'should'
 
 import sleep from '../core/sleep'
 
-describe('Dummy test case', async (): Promise<void> => {
+describe('Dummy test case', (): void => {
     it('Should do nothing asynchronously', async (): Promise<void> => {
         await sleep(10)
-        const spy: SinonSpy<any, any> = sinon.spy()
+        const spy: SinonSpy<string[], void> = sinon.stub()
         const arg = 'hello world'
         spy(arg)
         sinon.assert.calledOnce(spy)
